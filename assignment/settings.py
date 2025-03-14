f3a240ccf809f3441084248cde7a3af3e58dd0bf
PSG4160 <psg4160@gmail.com>
2025-03-14 11:56:40 +0900

    ADD: ALLOWED_HOSTS

--- assignment/settings.py
+++ assignment/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#8vy0xa#z&hc&w7(k&nz+xml&c48%iu@_qq^+(wh@zii)!9+px
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
